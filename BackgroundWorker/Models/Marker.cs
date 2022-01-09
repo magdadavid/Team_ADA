3acc76556cc8e5c7a27b3efe933e022398342fe7
David Magda <magda.david2018@gmail.com>
2022-01-09 16:14:18 +0200

    Revert "Revert "Update 3""
    This reverts commit 5791ce3178ea218f36e82273846e8a319e851958.

---
+++ BackgroundWorker/Models/Marker.cs
@@ -0,0 +1,19 @@
+
+
+using System;
+
+namespace Models{
+
+    public class Marker
+    {
+       
+        public Marker(){}
+        public string markeruser {get; set; }
+        
+        public string markerdate { get; set; }
+        public double markerlat { get; set; }
+        public double markerlong { get; set; }
+        public string markermess { get; set; }
+        
+    }
+}
